fa4c94f2ed5c14f8d97d9dd2ee93ace17b7c8105
Alex Blumenthaler <blumenthaleralex@gmail.com>
2020-06-18 17:39:30 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,12 @@
 def unsafe?(speed)
-  # if speed is unsafe this method is true
   if speed < 40 || speed > 60
     true
-  # else if speed is safe returns false
   else
     false
   end
 end
+
+speed < 40 || speed > 60? true: false
 
 
 
